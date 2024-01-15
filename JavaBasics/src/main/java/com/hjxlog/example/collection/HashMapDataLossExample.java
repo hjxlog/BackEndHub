e4e529cf483b14585a2a94413a088e1e0e69816d
HuangJX <hjxlog@gmail.com>
2024-01-15 17:25:35 +0800

    update

---
+++ JavaBasics/src/main/java/com/hjxlog/example/collection/HashMapDataLossExample.java
@@ -0,0 +1,37 @@
+package com.hjxlog.example.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDataLossExample {
+    private static final Map<String, Integer> hashMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        // 启动两个线程同时对HashMap进行写操作
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                hashMap.put("Key" + i, i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                hashMap.put("Key" + i, i * 10);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        // 等待两个线程执行完毕
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 打印HashMap的大小，预期结果是2000，但可能会小于这个值
+        System.out.println("HashMap size: " + hashMap.size());
+    }
+}
--- JavaBasics/src/main/java/com/hjxlog/example/collection/ListToMapExample.java
+++ JavaBasics/src/main/java/com/hjxlog/example/collection/ListToMapExample.java
@@ -3,6 +3,7 @@ package com.hjxlog.example.collection;
 import com.hjxlog.domain.Person;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,11 +18,13 @@ public class ListToMapExample {
         personList.add(new Person(3, "Charlie"));
         personList.add(new Person(1, "Elon"));
         // 将List转换为Map，以Person的ID为键，Person的NAME为值
-//        Map<Integer, String> personMap = personList.stream()
-//                .collect(Collectors.toMap(Person::getId, Person::getName));
-        // 将List转换为Map，以Person的ID为键，Person的NAME为值
-        Map<Integer, String> personMap = personList.stream()
-                .collect(Collectors.toMap(Person::getId, Person::getName, (existing, replacement) -> existing));
+        Map<Integer, String> personMap = new HashMap<>();
+        // 如果有相同的key，会报错IllegalStateException
+//        personMap = personList.stream().collect(Collectors.toMap(Person::getId, Person::getName));
+        // 可能有相同的key的，使用另外的  toMap(Function, Function, BinaryOperator)
+        personMap = personList.stream().collect(Collectors.toMap(Person::getId, Person::getName, (existing, replacement) -> existing));
+        // 如果不需要将结果按相遇顺序插入到Map中，使用toConcurrentMap(Function, Function)可以提供更好的并行性能。
+//        personMap = personList.stream().collect(Collectors.toConcurrentMap(Person::getId, Person::getName, (existing, replacement) -> existing));
         // 打印转换后的Map
         personMap.forEach((id, person) -> System.out.println("ID: " + id + ", Person: " + person));
     }
