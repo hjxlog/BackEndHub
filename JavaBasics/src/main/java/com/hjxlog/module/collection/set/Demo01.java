a011e1104b9a21777ea7147cbf6ae538f3ff21b3
HuangJX <hjxlog@gmail.com>
2024-07-22 14:02:00 +0800

    update

--- JavaBasics/src/main/java/com/hjxlog/module/collection/set/Demo01.java
+++ JavaBasics/src/main/java/com/hjxlog/module/collection/set/Demo01.java
@@ -1,4 +1,16 @@
 package com.hjxlog.module.collection.set;
 
+import java.util.*;
+
 public class Demo01 {
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.remove(null);
+        System.out.println(list);
+
+
+    }
+
 }
